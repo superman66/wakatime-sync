f4167780a4f6e59056f06fdee88abc646a2c6c85
Superman <supermanchc@gmail.com>
2018-08-22 23:30:50 +0800

    feat: 使用 node-schedule 每天定时跑任务

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,26 @@
+import moment from 'moment'
+const schedule = require('node-schedule')
 import GithubService from './service/GithubService'
 import WakatimeService from './service/WakatimeService'
+const config = require('../../config.json')
 
-const wakatimeInstance = new WakatimeService()
-const githubInstance = new GithubService()
+const wakatimeInstance = new WakatimeService(config.wakatimeApiKey)
+const githubInstance = new GithubService(config.gistToken)
 
-const date = '2018-08-19'
-const gistId = 'ec394723eae95cd864f978927bae305d'
-wakatimeInstance
-  .fetchSummaries(date)
-  .then(response => {
-    return githubInstance.updateGist(gistId, date, response)
-  })
-  .then(response => console.log('success'))
-  .catch(error => console.log(error))
+const syncWakaTimeToGist = function(gistId) {
+  const date = moment()
+    .subtract(1, 'days')
+    .format('YYYY-MM-DD')
+  wakatimeInstance
+    .fetchSummaries(date)
+    .then(response => {
+      return githubInstance.updateGist(gistId, date, response)
+    })
+    .then(response => console.log('success'))
+    .catch(error => console.log(error))
+}
+
+// 每天12点30分30秒执行该job
+const job = schedule.scheduleJob('30 * * * * *', function() {
+  syncWakaTimeToGist(config.syncGistId)
+})
