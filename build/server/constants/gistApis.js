05e03fe169a29a0ae2f271e7f36bdf2d352e8aa4
superman <supermanchc@gmail.com>
2018-12-10 16:19:34 +0800

    chore: remove build files

--- build/server/constants/gistApis.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var BASE_URL = 'https://api.github.com';
-
-var gist = exports.gist = BASE_URL + '/gists';
--- build/server/constants/wakatimeApis.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var BASE_URL = 'https://wakatime.com/api/v1';
-
-var summaries = exports.summaries = BASE_URL + '/users/current/summaries';
