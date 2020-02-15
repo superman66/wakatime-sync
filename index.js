47be38e91177bb132b4a3f22e5d0a2fce66b0044
superman <supermanchc@gmail.com>
2020-02-15 22:57:48 +0800

    fix: remove sendMessageToWechat type annotation

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ async function updateGist(date, content) {
  * @param {*} text 标题，最初256，必需
  * @param {*} desp 消息内容，最长64kb，可空
  */
-async function sendMessageToWechat(text: string, desp?: string) {
+async function sendMessageToWechat(text, desp) {
   if (typeof SCU_KEY !== 'undefined') {
     return Axios.get(`${scuPushApi}${SCU_KEY}.send`, {
       params: {
